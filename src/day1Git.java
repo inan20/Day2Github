7d196f7d31b7a43aa24b732b0d3636bb9abcd7fc
inan20 <ahmetinanyunefa@gmail.com>
2022-08-13 22:51:52 +0300

    V2

--- src/day1Git.java
+++ src/day1Git.java
@@ -1,5 +1,8 @@
 public class day1Git {
     public static void main(String[] args) {
         System.out.println( "hello git 1. deneme");
+        System.out.println( "hello git 2. deneme");
+        System.out.println( "hello git 3. deneme");
+
     }
 }
