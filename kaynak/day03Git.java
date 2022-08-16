30de5e033077e5598381cf1df70cc8f565a70e95
Ahmet İnan <110977687+inan20@users.noreply.github.com>
2022-08-17 00:18:01 +0300

    Create day03Git.java

--- kaynak/day03Git.java
+++ kaynak/day03Git.java
@@ -1,5 +1,6 @@
 public class day03Git {
     public static void main(String[] args) {
         System.out.println("github dosya gönderme çalışmaları" );
+        System.out.println("github dosya gönderme çalışmaları" );
     }
 }
