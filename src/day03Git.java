b6df50ef7325c183c6dd8f1730aaaf77d647a9b8
inan20 <ahmetinanyunefa@gmail.com>
2022-08-16 19:58:04 +0300

    v3

---
+++ src/day03Git.java
@@ -0,0 +1,5 @@
+public class day03Git {
+    public static void main(String[] args) {
+        System.out.println("github dosya gönderme çalışmaları" );
+    }
+}
---
+++ src/day2Git.java
@@ -0,0 +1,5 @@
+public class day2Git {
+    public static void main(String[] args) {
+        System.out.println("Bismillahirrahmenirrahim");
+    }
+}
